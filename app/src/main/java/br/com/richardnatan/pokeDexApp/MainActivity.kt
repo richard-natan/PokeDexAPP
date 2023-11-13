39ec27388e96f56b73f6d3573ea5772510ce2eb5
Richard Natan <rnjsilva@outlook.com>
2023-11-12 22:13:10 -0300

    A new return button has been added when performing a search; Some small refactorings were carried out to clean up the code; Fixed issues with colors in the dark theme; Small layout changes;

--- app/src/main/java/br/com/richardnatan/pokeDexApp/MainActivity.kt
+++ app/src/main/java/br/com/richardnatan/pokeDexApp/MainActivity.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.text.isDigitsOnly
 import androidx.recyclerview.widget.GridLayoutManager
@@ -22,54 +24,48 @@ class MainActivity : AppCompatActivity(), PokemonTask.Callback {
     private lateinit var adapter: Adapter
     private lateinit var rvMain: RecyclerView
     private lateinit var searchEdit: EditText
+    private lateinit var returnButton: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         searchEdit = findViewById(R.id.edit_search)
+        returnButton = findViewById(R.id.button_return)
         rvMain = findViewById(R.id.rv_main)
         adapter = Adapter(pokeList)
-
         rvMain.adapter = adapter
         rvMain.layoutManager = GridLayoutManager(this, 2)
 
         val pokemonTask = PokemonTask(this)
         pokemonTask.execute()
 
+        // Search Button
         searchEdit.setOnEditorActionListener { _, actionId, _ ->
-            if (actionId == EditorInfo.IME_ACTION_SEARCH){
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                if (searchEdit.text.isNotEmpty()) {
+                    returnButton.visibility = View.VISIBLE
+                    hideKeyboard(this.searchEdit)
+                }
+
                 if (cachedList.isEmpty()) {
                     cachedList.addAll(pokeList)
                 }
 
-                if (searchEdit.text.isNullOrEmpty()) {
-                    pokeList.clear()
-                    pokeList.addAll(cachedList)
-                    adapter.notifyDataSetChanged()
-                }
-
-
-                val list = mutableListOf<Pokemon>()
-
-                if (!searchEdit.text.isNullOrEmpty() && searchEdit.text.isDigitsOnly()) {
-                    val filterList = cachedList.filter { pokemon ->
-                        pokemon.id == searchEdit.text.toString().toInt()
-                    }
-                    list.addAll(filterList)
-                } else {
-                    val filterList = cachedList.filter { pokemon ->
-                        pokemon.name.contains(searchEdit.text.toString().lowercase())
-                    }
-                    list.addAll(filterList)
-                }
-
-
                 pokeList.clear()
-                pokeList.addAll(list)
+                pokeList.addAll(searchPokemon())
                 adapter.notifyDataSetChanged()
             }
             true
+        }
+
+        // Return Button
+        returnButton.setOnClickListener {
+            pokeList.clear()
+            pokeList.addAll(cachedList)
+            adapter.notifyDataSetChanged()
+            returnButton.visibility = View.GONE
+            searchEdit.text = null
         }
 
     }
@@ -99,7 +95,8 @@ class MainActivity : AppCompatActivity(), PokemonTask.Callback {
 
 
                 txtId.text = getString(R.string.pokemon_id, item.id.toString())
-                txtName.text = item.name.replaceFirst(item.name.first(), item.name.first().uppercaseChar())
+                txtName.text =
+                    item.name.replaceFirst(item.name.first(), item.name.first().uppercaseChar())
                 Picasso.get().load(item.picture).into(imageView)
 
             }
@@ -112,5 +109,35 @@ class MainActivity : AppCompatActivity(), PokemonTask.Callback {
         this.adapter.notifyDataSetChanged()
     }
 
+    private fun searchPokemon(): List<Pokemon> {
+        val filteredList = mutableListOf<Pokemon>()
 
+        // Filter by ID
+        if (!searchEdit.text.isNullOrEmpty() && searchEdit.text.isDigitsOnly()) {
+            val tempList = cachedList.filter { pokemon ->
+                pokemon.id == searchEdit.text.toString().toInt()
+            }
+            filteredList.addAll(tempList)
+        } else { // Filter by Name
+            val tempList = cachedList.filter { pokemon ->
+                pokemon.name.contains(searchEdit.text.toString().lowercase())
+            }
+            filteredList.addAll(tempList)
+        }
+
+        if (filteredList.isEmpty()) {
+            Toast.makeText(
+                this,
+                "No Pokémon found with the value: ${searchEdit.text}",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+
+        return filteredList
+    }
+
+    private fun hideKeyboard(view: View) {
+        val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+    }
 }
